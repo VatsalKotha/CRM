09e48c6063c99e7694c5df32210d7ff233f5ff53
VatsalKotha <vatsalkotha@gmail.com>
2023-05-24 17:17:57 +0530

    commit

--- lib/screens/home/report/meeting_page.dart
+++ lib/screens/home/report/meeting_page.dart
@@ -1,10 +1,7 @@
-import 'package:crm/screens/auth/database/fetch_meetings.dart';
+
+import 'package:crm/controllers/on_press_action.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:crm/screens/home/report/meeting_form.dart';
-import 'package:crm/utility/widget/meeting_card_widget.dart';
 
 class MeetingPage extends StatefulWidget {
   const MeetingPage({Key? key}) : super(key: key);
@@ -14,113 +11,73 @@ class MeetingPage extends StatefulWidget {
 }
 
 class _MeetingPageState extends State<MeetingPage> {
-  List<Map<String, dynamic>> meetingList = [];
-
-  late CalendarFormat _calendarFormat;
-  late DateTime _selectedDay;
-  late DateTime _focusedDay;
-  late DateTime _firstDay;
-  late DateTime _lastDay;
-  late Stream<List<Map<String, dynamic>>> _meetingsStream;
-
-  @override
-  void initState() {
-    super.initState();
-    _calendarFormat = CalendarFormat.month;
-    _selectedDay = DateTime.now();
-    _focusedDay = DateTime.now();
-    _firstDay = DateTime(2010);
-    _lastDay = DateTime(2050);
-    // _meetingsStream = _getMeetingsStream(_selectedDay);
-    fetchDatabaseList();
-  }
-
-  fetchDatabaseList() async {
-    List<Map<String, dynamic>> result = await FetchMeetings().getMeetings();
-
-    // ignore: unnecessary_null_comparison
-    if (result == null) {
-      print("Unable to retrieve");
-    } else {
-      setState(() {
-        meetingList = result;
-        // filteredLeadList = List.from(leadList);
-      });
-    }
-  }
-
-  // Stream<List<Map<String, dynamic>>> _getMeetingsStream(DateTime selectedDay) {
-  //   return FirebaseFirestore.instance
-  //       .collection('Meetings')
-  //       .where('Date', isEqualTo: Timestamp.fromDate(selectedDay))
-  //       .snapshots()
-  //       .map((QuerySnapshot querySnapshot) => querySnapshot.docs
-  //           .map((doc) => doc.data() as Map<String, dynamic>)
-  //           .toList());
-  // }
-
-  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
-    setState(() {
-      _selectedDay = selectedDay;
-      _focusedDay = focusedDay;
-      // _meetingsStream = _getMeetingsStream(selectedDay);
-    });
-  }
+  CalendarFormat calendarFormat = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TableCalendar(
-            calendarFormat: _calendarFormat,
-            focusedDay: _focusedDay,
-            firstDay: _firstDay,
-            lastDay: _lastDay,
-            onFormatChanged: (format) {
-              setState(() {
-                _calendarFormat = format;
-              });
-            },
-            onPageChanged: (focusedDay) {
-              setState(() {
-                _focusedDay = focusedDay;
-              });
-            },
-            onDaySelected: _onDaySelected,
+      body: TableCalendar(
+        focusedDay: selectedDay,
+        firstDay: DateTime(2010),
+        lastDay: DateTime(2050),
+        calendarFormat: calendarFormat,
+        startingDayOfWeek: StartingDayOfWeek.monday,
+        headerStyle: HeaderStyle(
+            formatButtonVisible: true,
+            formatButtonShowsNext: false,
+            formatButtonDecoration: BoxDecoration(
+              color: const Color(0xFFB3E5FC),
+              borderRadius: BorderRadius.circular(8.0),
+            )
+        ),
+
+        // to style the calendar
+        calendarStyle: const CalendarStyle(
+
+          isTodayHighlighted: true,
+          // decoration for the current date
+          todayDecoration: BoxDecoration(
+            color: Color(0xFFFFB466),
+            shape: BoxShape.circle,
           ),
-          const SizedBox(height: 16),
-          Text(
-            'Meetings for ${DateFormat('yyyy-MM-dd').format(_selectedDay)}',
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          todayTextStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
-          const SizedBox(height: 8),
-          Expanded(
-            child: ListView.builder(
-              itemCount: meetingList.length,
-              itemBuilder: (context, index) {
-                final data = meetingList[index];
-                return MeetingCardWidget(
-                  title: data['Title'],
-                  clientFirstName: data['Client First Name'],
-                  clientLastName: data['Client Last Name'],
-                  salesPerson: data['Sales Person'],
-                  description: data['Description'],
-                );
-              },
-            ),
+
+          // decoration for the selected date
+          selectedDecoration: BoxDecoration(
+            color: Color(0xFFE4EFFA),
+            shape: BoxShape.circle,
           ),
-        ],
+          selectedTextStyle: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+
+        selectedDayPredicate: (DateTime inputDate) {
+          return isSameDay(selectedDay, inputDate);
+        },
+        onFormatChanged: (CalendarFormat inputFormat) {
+          setState(() {
+            calendarFormat = inputFormat;
+          });
+        },
+        onDaySelected: (DateTime inputSelectedDay, DateTime inputFocusedDay) {
+          setState(() {
+            selectedDay = inputSelectedDay;
+            focusedDay = inputFocusedDay;
+          });
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const MeetingForm()),
-          );
-        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
         child: const Icon(Icons.add),
+        onPressed: () => OnPressAction.goToMeetingFrom(),
       ),
     );
   }
