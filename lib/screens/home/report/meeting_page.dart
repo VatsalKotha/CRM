9fbaf2193247c8c8958ce324fa2e767a7ef6d7da
VatsalKotha <vatsalkotha@gmail.com>
2023-05-26 16:03:01 +0530

    commit

--- lib/screens/home/report/meeting_page.dart
+++ lib/screens/home/report/meeting_page.dart
@@ -1,5 +1,6 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crm/controllers/on_press_action.dart';
+import 'package:crm/utility/widget/meeting_card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -11,66 +12,102 @@ class MeetingPage extends StatefulWidget {
 }
 
 class _MeetingPageState extends State<MeetingPage> {
+  List<dynamic> events = [];
+  DateTime selectedDate = DateTime.now();
   CalendarFormat calendarFormat = CalendarFormat.month;
-  DateTime selectedDay = DateTime.now();
-  DateTime focusedDay = DateTime.now();
+  Map<DateTime, List<dynamic>> eventsByDate = {};
+
+  @override
+  void initState() {
+    super.initState();
+    fetchEvents();
+  }
+
+  Future<void> fetchEvents() async {
+    try {
+      final QuerySnapshot<Map<String, dynamic>> snapshot =
+          await FirebaseFirestore.instance.collection('Meetings').get();
+
+      final List<dynamic> fetchedEvents = snapshot.docs
+          .map((doc) => {
+                ...doc.data(),
+                'Date': DateTime.parse(doc['Date']),
+              })
+          .toList();
+
+      setState(() {
+        events = fetchedEvents;
+        eventsByDate = groupEventsByDate(fetchedEvents);
+      });
+    } catch (e) {
+      print('Error fetching events: $e');
+    }
+  }
+
+  Map<DateTime, List<dynamic>> groupEventsByDate(List<dynamic> events) {
+    final Map<DateTime, List<dynamic>> eventsByDate = {};
+
+    for (final event in events) {
+      final DateTime eventDate = event['Date'];
+
+      if (eventsByDate.containsKey(eventDate)) {
+        eventsByDate[eventDate]!.add(event);
+      } else {
+        eventsByDate[eventDate] = [event];
+      }
+    }
+
+    return eventsByDate;
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TableCalendar(
-        focusedDay: selectedDay,
-        firstDay: DateTime(2010),
-        lastDay: DateTime(2050),
-        calendarFormat: calendarFormat,
-        startingDayOfWeek: StartingDayOfWeek.monday,
-        headerStyle: HeaderStyle(
-            formatButtonVisible: true,
-            formatButtonShowsNext: false,
-            formatButtonDecoration: BoxDecoration(
-              color: const Color(0xFFB3E5FC),
-              borderRadius: BorderRadius.circular(8.0),
-            )
-        ),
-
-        // to style the calendar
-        calendarStyle: const CalendarStyle(
-
-          isTodayHighlighted: true,
-          // decoration for the current date
-          todayDecoration: BoxDecoration(
-            color: Color(0xFFFFB466),
-            shape: BoxShape.circle,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2022),
+            lastDay: DateTime.utc(2024),
+            focusedDay: selectedDate,
+            calendarFormat: calendarFormat,
+            onFormatChanged: (format) {
+              setState(() {
+                calendarFormat = format;
+              });
+            },
+            onPageChanged: (date) {
+              setState(() {
+                selectedDate = date;
+              });
+            },
+            eventLoader: (date) {
+              return eventsByDate[date] ?? [];
+            },
+            onDaySelected: onDaySelected,
           ),
-          todayTextStyle: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+          Expanded(
+            child: ListView.builder(
+              itemCount: eventsByDate[selectedDate]?.length ?? 0,
+              itemBuilder: (context, index) {
+                final event = eventsByDate[selectedDate]?[index];
+                return MeetingCardWidget(
+                  title: event?['Title'] ?? '',
+                  clientFirstName: event?['Client First Name'] ?? '',
+                  clientLastName: event?['Client Last Name'] ?? '',
+                  salesPerson: event?['Sales Person'] ?? '',
+                  description: event?['Description'] ?? '',
+                );
+              },
+            ),
           ),
-
-          // decoration for the selected date
-          selectedDecoration: BoxDecoration(
-            color: Color(0xFFE4EFFA),
-            shape: BoxShape.circle,
-          ),
-          selectedTextStyle: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-
-        selectedDayPredicate: (DateTime inputDate) {
-          return isSameDay(selectedDay, inputDate);
-        },
-        onFormatChanged: (CalendarFormat inputFormat) {
-          setState(() {
-            calendarFormat = inputFormat;
-          });
-        },
-        onDaySelected: (DateTime inputSelectedDay, DateTime inputFocusedDay) {
-          setState(() {
-            selectedDay = inputSelectedDay;
-            focusedDay = inputFocusedDay;
-          });
-        },
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         shape: RoundedRectangleBorder(
